6b5ec88c23140a5913f513caa5d8571b0f01d0b8
Cody Swartz <ctsstc@gmail.com>
2018-05-10 20:37:09 -0700

    just white spacing

--- hw5.asm
+++ hw5.asm
@@ -1,28 +1,28 @@
 section .text
 global _start       ; for linker
 
-_start:;            label
-jmp _writeCrap;
-jmp _exit;
+_start:             ; label
+jmp _writeCrap
+jmp _exit
 
 _writeCrap:
-mov eax, 4;         syscall #4 - write
-mov ebx, 1;         std out
-mov ecx, msg;       buffer out
-mov edx, SIZE;      buffer size
-int 0x80; 
+mov eax, 4          ; syscall #4 - write
+mov ebx, 1          ; std out
+mov ecx, msg        ; buffer out
+mov edx, SIZE       ; buffer size
+int 0x80            ; invoke dispatcher
 
 _readCrap:
 mov eax, 3          ; syscall 3 - read
 mov ebx, 0          ; std in
 mov ecx, buffer     ; buffer to read into
-mov edx, INSIZE       ; number of bytes to read
+mov edx, INSIZE     ; number of bytes to read
 int 0x80            ; invoke dispatcher
 
 _exit:
-mov eax, 1; 
-mov ebx, 0; 
-int 0x80; 
+mov eax, 1
+mov ebx, 0
+int 0x80
 
 ;;; Variables
 section .data
@@ -30,5 +30,5 @@ msg: db "Hello World", 10
 SIZE: equ $ - msg
 
 section .bss
-INSIZE: equ 1024      ; declare constant
-buffer: resb INSIZE   ; declare buffer of SIZE bytes
+INSIZE: equ 1024    ; declare constant
+buffer: resb INSIZE ; declare buffer of SIZE bytes
